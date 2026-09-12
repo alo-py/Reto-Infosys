d46fd367d63225dab59e062051c9c7a0cba43d55
NarcisoDMC <davidmadefukingtroll810@gmail.com>
2026-09-12 09:03:11 -0600

    feat(frontend): agregar componentes ui, autenticacion y assets

--- frontend/app/(Home)/layout.tsx
+++ frontend/app/(Home)/layout.tsx
@@ -1,7 +1,9 @@
+import Navbar from "@/app/ui/Components/Home/Navbar";
 
 export default function HomeLayout({ children }: LayoutProps<"/">) {
     return (
         <div className="flex flex-col min-h-full">
+            <Navbar />
             {children}
         </div>
     );
